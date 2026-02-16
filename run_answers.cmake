4855e07a68bf47abc8ef1347458fa0dd87ab679c
okuoku <mjt@cltn.org>
2026-02-16 11:42:40 +0900

    Update test runner

--- run_answers.cmake
+++ run_answers.cmake
@@ -7,22 +7,24 @@ set(projroot ${CMAKE_CURRENT_LIST_DIR}/__dummy)
 execute_process(COMMAND
     ${Python_EXECUTABLE} ${myroot}/analyze.py
     INPUT_FILE ${projroot}/compile_commands.json
-    OUTPUT_FILE rs_project_cmds.jsonl
+    OUTPUT_FILE ${projroot}/rs_project_cmds.jsonl
 )
 
 execute_process(COMMAND
     ${Python_EXECUTABLE} ${myroot}/inject_preprocess.py
     -b check
-    INPUT_FILE rs_project_cmds.jsonl
-    OUTPUT_FILE rs_project_preprocess.jsonl
+    INPUT_FILE ${projroot}/rs_project_cmds.jsonl
+    OUTPUT_FILE ${projroot}/rs_project_preprocess.jsonl
 )
 
 execute_process(COMMAND
     ${Python_EXECUTABLE} ${myroot}/exec_project.py
-    INPUT_FILE rs_project_preprocess.jsonl
+    INPUT_FILE ${projroot}/rs_project_preprocess.jsonl
+    WORKING_DIRECTORY ${projroot}
 )
 
 execute_process(COMMAND
     ${Python_EXECUTABLE} ${myroot}/analyze_include_tree.py
-    INPUT_FILE rs_project_preprocess.jsonl
+    INPUT_FILE ${projroot}/rs_project_preprocess.jsonl
+    WORKING_DIRECTORY ${projroot}
 )
